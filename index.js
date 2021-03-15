a2a528df0d0ff2a92d7467d5914210daec8727fc
EECOLOR <eecolor@qirx.org>
2021-03-15 21:47:04 +0100

    Add readme

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 export { asRouteMap, pick, pickRoute, asRouteChain, routeSymbol } from './src/routeMap'
-export { useNavigate, useRoute, usePick, useRouting, useMatch, Link, LocationProvider, useRoutes } from './src/routing'
+export { useNavigate, useRoute, usePick, useRouting, useMatch, Link, LocationProvider, useRoutes, useLocation, useHistory, useRouteMap } from './src/routing'
